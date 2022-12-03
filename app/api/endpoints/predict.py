b8694c220af3941422964a27779843bca08d0fec
louison <iwa-30@sr.dmcs.pl>
2022-12-03 16:30:56 +0100

    Ajout du parser pour anonymiser les phrases

--- app/api/endpoints/predict.py
+++ app/api/endpoints/predict.py
@@ -5,13 +5,13 @@ router = APIRouter()
 
 
 @router.post("/api/predict")
-async def predict_anonymous_data(texte : str):
+async def predict_anonymous_data(sentence : str):
     return {"Le modèle a prédit que ce texte contient ces données sensibles" :"balalala" }
 
 @router.post("/api/predict/tag")
-async def replace_text_by_tag(texte : str):
+async def replace_text_by_tag(sentence : str):
     return {"Le modèle a prédit que ce texte contient ces données sensibles" :"balalala" }
 
 @router.post("/api/predict/fakedata")
-async def replace_text_by_fakedata(texte : str):
+async def replace_text_by_fakedata(sentence : str):
     return {"Le modèle a prédit que ce texte contient ces données sensibles" :"balalala" }
---
+++ app/model/Parser.py
@@ -0,0 +1,39 @@
+import re
+from transformers import AutoTokenizer, AutoModelForTokenClassification, pipeline
+
+class Parser:
+
+    nlp : pipeline
+    customFilters : list
+
+
+    def __init__(self, customFilters: list) -> None:
+        tokenizer = AutoTokenizer.from_pretrained("Jean-Baptiste/camembert-ner-with-dates")
+        model = AutoModelForTokenClassification.from_pretrained("Jean-Baptiste/camembert-ner-with-dates")
+        self.nlp = pipeline('ner', model=model, tokenizer=tokenizer, aggregation_strategy="simple")
+
+        self.customFilters = customFilters
+
+    def _intermediate_parser(self, sentence : str, processedList: list, regex : str, tag : str):
+        reg = re.compile(regex)
+
+        s = reg.search(sentence)
+
+        if s:
+            newList = [{'entity_group': tag, 'score': 1.0, 'word': sentence[s.start(): s.end()], 'start': s.start(), 'end': s.end()}]
+            for e in processedList:
+                # Check if there is overlapping between MAIL and other tags
+                if not (e['start'] >= newList[0]['start'] and e['end'] <= newList[0]['end']):
+                    newList.append(e)
+                    
+                return newList
+
+    def parse(self, sentence : str):
+        processedList = self.nlp(sentence)
+
+        for filter in self.customFilters:
+            processedList = self._intermediate_parser(sentence, processedList, filter['regex'], filter['tag'])
+
+        #White list
+
+        return self.nlp(sentence)
